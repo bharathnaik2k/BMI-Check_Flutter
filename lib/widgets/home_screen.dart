cbd76b2b96b5c4e5ee6b54f3dad97aca5aa8d595
Bharath Naik <bharathnaik868@gmail.com>
2024-10-24 15:03:34 +0530

    update

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -9,6 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController weightcontroller = TextEditingController();
+  TextEditingController heightcontroller = TextEditingController();
+
+  double? bmiData;
+
+  calculate() {
+    double height = (int.parse(heightcontroller.text).toInt() / 100) * 2;
+    double result = int.parse(weightcontroller.text).toInt() / height;
+    bmiData = result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,8 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Image.asset(
@@ -78,100 +88,168 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontSize: 18,
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 15),
             Container(
-              padding: const EdgeInsets.all(8),
+              padding: const EdgeInsets.all(14.0),
               decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(5),
+                borderRadius: BorderRadius.circular(15),
+                color: const Color.fromARGB(255, 192, 122, 0),
               ),
-              child: Row(
+              child: Column(
                 children: [
-                  const Text(
-                    "Enter Your Weight",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          "assets/svg/weight.svg",
+                          height: 26,
+                          width: 26,
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          "Enter Your Weight :",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Expanded(
+                          child: TextField(
+                            controller: weightcontroller,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              fillColor: Colors.grey.shade300,
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                right: 16,
+                                left: 16,
+                              ),
+                              hintText: "Kg",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(width: 12),
-                  SvgPicture.asset(
-                    "assets/svg/weight.svg",
-                    height: 30,
-                    width: 30,
+                  const SizedBox(height: 12),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          "assets/svg/height.svg",
+                          height: 26,
+                          width: 26,
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          "Enter Your Height  :",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Expanded(
+                          child: TextField(
+                            controller: heightcontroller,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              fillColor: Colors.grey.shade300,
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                right: 16,
+                                left: 16,
+                              ),
+                              hintText: "Cm",
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 5),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        contentPadding: const EdgeInsets.only(
-                          right: 16,
-                          left: 16,
-                        ),
-                        hintText: "Kg",
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                  const SizedBox(height: 12),
+                  TextButton(
+                    style: const ButtonStyle(
+                      minimumSize: MaterialStatePropertyAll(
+                        Size(180, 40),
                       ),
+                      backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 0, 145, 5),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (heightcontroller.text.isNotEmpty &&
+                          weightcontroller.text.isNotEmpty) {
+                        setState(() {
+                          calculate();
+                        });
+                      }
+                    },
+                    child: const Text(
+                      "Check",
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 12),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(5),
+            const SizedBox(height: 5),
+            const Text(
+              "Healthy BMI Range : 18.5 kg/m² - 25 kg/m²",
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 145, 5),
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
               ),
-              child: Row(
+            ),
+            const SizedBox(height: 15),
+            Container(
+              width: 160,
+              height: 160,
+              decoration: const BoxDecoration(),
+              child: Stack(
                 children: [
-                  const Text(
-                    "Enter Your Height",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          bmiData!.toStringAsFixed(2),
+                          style: const TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                         Text("data")
+                      ],
                     ),
                   ),
-                  const SizedBox(width: 12),
-                  SvgPicture.asset(
-                    "assets/svg/height.svg",
-                    height: 30,
-                    width: 30,
-                  ),
-                  const SizedBox(width: 5),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        contentPadding: const EdgeInsets.only(
-                          right: 16,
-                          left: 16,
-                        ),
-                        hintText: "Cm",
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                      ),
+                  Opacity(
+                    opacity: 0.2,
+                    child: SvgPicture.asset(
+                      "assets/svg/normalweight.svg",
                     ),
                   ),
                 ],
               ),
-            ),
-            TextButton(
-              style: const ButtonStyle(
-                fixedSize: MaterialStatePropertyAll(
-                  Size(double.infinity, 20),
-                ),
-              ),
-              onPressed: () {},
-              child: const Text("Check"),
             ),
           ],
         ),
